8a61ce5430c019128220a738a1e86f39ee8f0669
Robyn Speer <rspeer@luminoso.com>
2018-09-28 12:13:52 -0400

    Update my name

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 
 # Data about this site
-BLOG_AUTHOR = "Rob Speer"  # (translatable)
+BLOG_AUTHOR = "Robyn Speer"  # (translatable)
 BLOG_TITLE = "ConceptNet blog"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
